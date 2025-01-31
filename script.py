cfe0d5050d7a0698071dbc1f6176efdc152ad63f
Tommy-Las <70454026+Tommy-Las@users.noreply.github.com>
2025-01-31 20:21:31 +0000

    exported model and prepared data for testing in automated script

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import os
-
+import joblib
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -114,6 +114,8 @@ def process_and_merge_dfs(df_gps, df_speed):
         # Perform an inner join on matching DATE and PLAYER/ID values
         merged_df = df_gps_filtered.merge(df_speed, left_on=['DATE', 'PLAYER'], right_on=['DATE', 'ID'], how='inner')
 
+        merged_df.reset_index(drop=True, inplace=True)
+
         print("DataFrames processed and merged successfully.")
         
         return merged_df
@@ -132,15 +134,18 @@ def process_and_merge_dfs(df_gps, df_speed):
         print(f"An error occurred while processing and merging DataFrames: {e}")
         return None
 
-def data_processing():
+def data_processing(df):
     cols = ['Total D', '>19.8', '> 25 Km/h', 'ACC',
        'DEC', 'ID', 'Max Speed', 'Sprints', 'MINUTES', 'Max Speed Season',
        'Avg Speed Season', '% Max Speed',
        '%Speed diference against max. Speed average']
     
-    merged_df[cols] = merged_df[cols].astype(float)
-    merged_df.rename(columns=column_rename_dict, inplace=True)
-    merged_df['Session'] = merged_df['Session'].str.replace(' ', '', regex=False)
+    df[cols] = df[cols].astype(float)
+    df.rename(columns=column_rename_dict, inplace=True)
+    
+    df = session_OHE(df)
+
+    return df
 
 def calcular_acumulado(df, columnas_calcular, dias):
     # Columns to exclude when the day is 3
@@ -200,10 +205,28 @@ def calcular_acumulado(df, columnas_calcular, dias):
 
     return df_resultado
 
-def calculate_fatigue_metrics(df, metrics):
+def filter_players(df):
+    # Step 1: Select the rows with the latest date
+    latest_date = df['DATE'].max()
+    latest_date_rows = df[df['DATE'] == latest_date]
 
+    # Step 2: Iterate through each row to check TD, TD-3, and TD7 values
+    for index, row in latest_date_rows.iterrows():
+        if row['TD'] == row['TD-3'] == row['TD-7']:
+            # Print PlayerID and drop the row
+            print(f"PlayerID {row['PlayerID']} does not have enough information.")
+            df = df.drop(index)
+
+    return df
+
+def calculate_fatigue_metrics(df, metrics):
+    min_threshold = 1e-5  # Small constant to replace zeros
+    
     # Calculate ACWR, MSWR for each metric
     for metric in metrics:
+        # Replace zeroes with min value to avoid division by 0
+        df[f'{metric}-21-avg'] = df[f'{metric}-21-avg'].replace(0, min_threshold)
+        df[f'{metric}-7-std'] = df[f'{metric}-7-std'].replace(0, min_threshold)
 
         # Calculate 7-day and 28-day averages for ACWR
         df[f'{metric}_ACWR'] = df[f'{metric}-7-avg'] / df[f'{metric}-21-avg']
@@ -216,26 +239,35 @@ def calculate_fatigue_metrics(df, metrics):
     return df
 
 def session_OHE(df):
-    # Encoding logic
-    # Step 1: Replace ['MD', 'MD(HOME)', 'MD(AWAY)'] with 'MD'
+    # Strip spaces
+    df['Session'] = df['Session'].str.replace(' ', '', regex=False)
+    
+    # Replace ['MD', 'MD(HOME)', 'MD(AWAY)'] with 'MD'
     md_values = ['MD', 'MD(HOME)', 'MD(AWAY)']
     df['Session'] = df['Session'].replace(md_values, 'MD')
 
-    # Step 2: Sort by PlayerID and Date
-    df.sort_values(by=['PlayerID', 'DATE'], inplace=True)
+    # Perform one-hot encoding
+    encoded_df = pd.get_dummies(df, columns=['Session'], prefix='Session')
 
-    # Step 3: Shift the Session column by -1 for each PlayerID
-    df['Session'] = df.groupby('PlayerID')['Session'].shift(1)
+    one_hot_columns = [col for col in encoded_df.columns if col.startswith('Session_')]
+    encoded_df[one_hot_columns] = encoded_df[one_hot_columns].astype(int)
 
-    # Step 2: Perform one-hot encoding
-    final_df = pd.get_dummies(df, columns=['Session'], prefix='Session')
+    #encoded_df = final_df.dropna(subset=one_hot_columns)
+    return encoded_df
 
-    one_hot_columns = [col for col in final_df.columns if col.startswith('Session_')]
-    final_df[one_hot_columns] = final_df[one_hot_columns].astype(int)
+def process_data_testing(df):
+    latest_date = df['DATE'].max()
+    latest_date_rows = df[df['DATE'] == latest_date]
 
-    final_df = final_df.dropna(subset=one_hot_columns)
+    columns_to_rename = ['TD', '>19.8', '>25', 'ACC', 'DEC', 'Sprints', 'Mins']
 
-    return final_df
+    # Rename columns by adding -1
+    latest_date_rows.rename(columns={col: f"{col}-1" for col in columns_to_rename}, inplace=True)
+    
+    return latest_date_rows
+
+cols_calculate_loads = ['TD', '>19.8', '>25', 'ACC', 'DEC', 'Sprints', '% Max Speed']
+
 
 cols_calculate_fatigues= ['TD', '>19.8', '>25', 'ACC', 'DEC']
 
@@ -271,30 +303,22 @@ if __name__ == "__main__":
     # Process and merge DataFrames
     merged_df = process_and_merge_dfs(df_gps, df_speed)
         
-    data_processing()
+    merged_df = data_processing(merged_df)
 
     merged_df.info()
 
-    cols_calculate = ['TD', '>19.8', '>25', 'ACC', 'DEC', 'Sprints', 'Mins', 'TD_Rel', '>19.8_Rel', '>25_Rel',
-       'ACC_Rel', 'DEC_Rel', '% Max Speed']
+    cumulative_df = calcular_acumulado(merged_df, cols_calculate_loads, [3,7,21])
+    
+    # Filter players with enough data
 
-    cols_calculate = ['TD', '>19.8', '>25', 'ACC', 'DEC', 'Sprints', '% Max Speed']
+    filtered_df = filter_players(cumulative_df)
 
-    cumulative_df = calcular_acumulado(merged_df, cols_calculate, [3,7,21])
+    complete_df = calculate_fatigue_metrics(filtered_df,  cols_calculate_fatigues)
 
-    complete_df = calculate_fatigue_metrics(cumulative_df,  cols_calculate_fatigues)
+    test_df = process_data_testing(complete_df)
 
-    latest_date = cumulative_df['DATE'].max()
-    latest_date_rows = cumulative_df[cumulative_df['DATE'] == latest_date]
-
-    columns_to_rename = ['TD', '>19.8', '>25', 'ACC', 'DEC', 'Sprints', 'Mins']
-
-    # Rename columns by adding -1
-    latest_date_rows.rename(columns={col: f"{col}-1" for col in columns_to_rename}, inplace=True)
-
-    metrics_train = ['Injury','TD-1', '>19.8-1', '>25-1', 'ACC-1', 'DEC-1', 'Sprints-1',
-       'Mins-1', 'TD_Rel-1', '>19.8_Rel-1', '>25_Rel-1', 'ACC_Rel-1',
-       'DEC_Rel-1', '% Max Speed-1', 'TD-3', '>19.8-3', '>25-3', 'ACC-3',
+    metrics_train = ['TD-1', '>19.8-1', '>25-1', 'ACC-1', 'DEC-1', 'Sprints-1',
+       'Mins-1', '% Max Speed-1', 'TD-3', '>19.8-3', '>25-3', 'ACC-3',
        'DEC-3', 'Sprints-3', 'TD-7', '>19.8-7', '>25-7', 'ACC-7',
        'DEC-7', 'Sprints-7', 'TD-21', '>19.8-21', '>25-21', 'ACC-21',
        'DEC-21', 'Sprints-21', 'TD_ACWR', 'TD_MSWR', '>19.8_ACWR',
@@ -303,5 +327,9 @@ if __name__ == "__main__":
        'Session_M-1', 'Session_M-2', 'Session_M-3', 'Session_M-4',
        'Session_M-5', 'Session_MD']
 
+    # Load the classifier
+    model = joblib.load('xgb_classifier_model.pkl')
 
+    # predictions = model.predict(X_test)  # Assuming X_test is your test data
+    # print(predictions)
 
